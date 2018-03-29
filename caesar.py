aff7db03dc06ddd9363ca8a1b4be7704e4ccf79a
Hugo Hoffman <Hugo@Hugos-MacBook-Pro.local>
2018-03-29 14:01:01 -0500

    debugged

--- caesar.py
+++ caesar.py
@@ -11,7 +11,11 @@ def rotate_character(char, rot):
         while alphabet_position(char) + adj_rot > 25:
             adj_rot = adj_rot - 26
         return chr(adj_rot + ord(char))
-    if 96<ord(char) and 123>ord(char):
+    if ord(char)>96 and ord(char)<123:
+        if ord(char) + rot>122:
+            return chr(rot + ord(char) - 26)
+        return chr(ord(char) + rot)
+    if ord(char)>64 and ord(char)<91:
         if ord(char) + rot>122:
             return chr(rot + ord(char) - 26)
         return chr(ord(char) + rot)
